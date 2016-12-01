7756a75fc43fbcd5b5ee974cbf69700dfef70e06
Jacob Vander Schel <jacob-vanderschel@uiowa.edu>
2016-12-01 05:50:46 +0000

    created user profile page

--- php/profile.php
+++ php/profile.php
@@ -1,5 +1,6 @@
+<?php session_start(); ?>
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="en">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -19,6 +20,54 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
+        /* Call Database */
+	    define('DB_SERVER', 'localhost');
+	    define('DB_USERNAME', 'root');
+	    define('DB_PASSWORD', '');
+	    define('DB_DATABASE', 'vote');
+	    $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+        
+        /* Check that connection set up successful */
+	    if(!$con){
+		    echo "Couldn't connect to database";
+		    echo "Err no." . mysqli_connect_errno() . PHP_EOL;
+	    }
+	    
+	    // GET user associated with user id
+			$get_user = "SELECT u.* FROM users as u
+									where u.id = '" . $_SESSION["voterID"] . "'";
+
+			$res = $con->query($get_user);
+			    $user_info = [];
+			
+			if($res->num_rows > 0){
+                //output data of each row
+				$row = $res->fetch_assoc();
+				echo "<table><tr><th>User Info</th></tr>";
+				echo "<tr><td>". "Voter ID:" . "</td><td>" . $row["id"] . "</td></tr>";
+				echo "<tr><td>". "Full Name:" . "</td><td>" . row["full_name"] . "</td></tr>";
+				echo "<tr><td>". "Email:" . "</td><td>" . row["email"] . "</td></tr>";
+				echo "<tr><td>". "Password:" . "</td><td>" . row["password"] . "</td></tr>";
+				echo "<tr><td>". "License Number:" . "</td><td>" . row["license_nmbr"] . "</td></tr>";
+				echo "<tr><td>". "Address:" . "</td><td>" . row["address"] . "</td></tr>";
+				echo "<tr><td>". "Zip Code:" . "</td><td>" . row["zipcode"] . "</td></tr>";
+				echo "<tr><td>". "State:" . "</td><td>" . row["state"] . "</td></tr>";
+				echo "<tr><td>". "Date of Birth:" . "</td><td>" . row["dob"] . "</td></tr>";
+				echo "<tr><td>". "Gender:" . "</td><td>" . row["gender"] . "</td></tr>";
+				echo "<tr><td>". "Party Affiliation:" . "</td><td>" . row["party_aff"] . "</td></tr>";
+				echo "<tr><td>". "SSN:" . "</td><td>" . row["ssn"] . "</td></tr>";
+				echo "<tr><td>". "Passport Number:" . "</td><td>" . row["passport_nmbr"] . "</td></tr>";
+				
+				echo "</table>";
+			}
+			else {
+			    echo "no results";
+			}
+			
+			$con->close();
+	    
+	    
+        
         <!-- Add your site or application content here -->
         <p>Hello world! This is HTML5 Boilerplate.</p>
 
