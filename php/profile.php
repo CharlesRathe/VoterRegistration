a5f124d062873c81424a4f80fd6075383c6ae0f2
Jacob Vander Schel <jacob-vanderschel@uiowa.edu>
2016-12-01 05:55:16 +0000

    commented out helloworld from user page

--- php/profile.php
+++ php/profile.php
@@ -69,7 +69,7 @@
 	    
         
         <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <!-- <p>Hello world! This is HTML5 Boilerplate.</p> -->
 
     </body>
 </html>
